b244bc0fed55f8adf5f10c8d4de91ec3a41235ab
magicmarie <31403932+magicmarie@users.noreply.github.com>
2017-11-05 14:45:22 +0300

    Importing other flask extensions

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,22 @@
 # local imports
 from app.models.recipeitem import Recipe
-from app.models.users import user
-from app.model.yummyrecipeapp import Yummy
+from app.models.users import User
+from app.models.yummyrecipesapp import Yummy
 
 # third party imports
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, validators
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
+    """Users log into their accounts """
     email = StringField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
 
 
 class SignupForm(FlaskForm):
+    """ This allows users to create accounts"""
     email = StringField('Email:', validators=[DataRequired()])
     password = PasswordField('New Password', [validators.Required(
     ), validators.EqualTo('confirm', message='Passwords must match')])
@@ -22,9 +24,11 @@ class SignupForm(FlaskForm):
 
 
 class CategoryForm(FlaskForm):
+    """creates a category """
     Category = StringField('Category:', validators=[DataRequired()])
 
 
 class RecipeForm(FlaskForm):
+    """ creates a recipe item"""
     name = StringField('Recipe:', validators=[DataRequired()])
     description = StringField('Description:', validators=[DataRequired()])
