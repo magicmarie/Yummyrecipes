9cd645633c5d848a77134da923b79eca2777ddc7
magicmarie <31403932+magicmarie@users.noreply.github.com>
2017-11-05 04:35:06 +0300

    Added form classes

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,30 @@
 # local imports
 from app.models.recipeitem import Recipe
 from app.models.users import user
-from app.modela.yummyrecipeapp import Yummy
+from app.model.yummyrecipeapp import Yummy
 
 # third party imports
-from flask import flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
+class SignupForm(FlaskForm):
+    email = StringField('Email:', validators=[DataRequired()])
+    password = PasswordField('New Password', [validators.Required(
+    ), validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+
+
+class CategoryForm(FlaskForm):
+    Category = StringField('Category:', validators=[DataRequired()])
+
+
+class RecipeForm(FlaskForm):
+    name = StringField('Recipe:', validators=[DataRequired()])
+    description = StringField('Description:', validators=[DataRequired()])
